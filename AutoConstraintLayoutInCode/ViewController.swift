efbeb1c746b85d211ab13615cce6404774945956
Wesley Osborne <wesleyosborne1@gmail.com>
2021-02-23 12:21:19 -0500

    Initial Commit

---
+++ AutoConstraintLayoutInCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoConstraintLayoutInCode
+//
+//  Created by Wesley Osborne on 2/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
